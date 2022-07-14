6fc8a7162a5576eef678e44e01882129bef0f416
God'spower <westcode6@gmail.com>
2022-07-14 13:09:53 +0100

    new

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ import SplashScreen from "./pages/Onboarding/splashScreen";
 import SplashScreen2 from "./pages/Onboarding/splashScreen2";
 import SplashScreen3 from "./pages/Onboarding/splashScreen3";
 
-
 function App() {
   return (
     <React.Fragment>
       <Routes>
+        <Route path="/" element={<SplashScreen />} />
+
         <Route path="/home" element={<Home />} />
 
-        <Route path="/splashScreen" element={<SplashScreen />} />
         <Route path="/splashScreen2" element={<SplashScreen2 />} />
 
         <Route path="/splashScreen3" element={<SplashScreen3 />} />
-
-
       </Routes>
       {/* <BottomNavbar /> */}
     </React.Fragment>
